5cbf713c569aa06590bd48d90b85977b6e4c271d
dnxa <kuridze0@gmail.com>
2024-12-15 19:02:20 +0400

    First commit

---
+++ l22/product/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import get_product_view, delete_product_view, create_product_view
+
+urlpatterns = {
+    path('<int:product_id>', get_product_view, name='get_product_view'),
+    path('delete/<int:product_id>', delete_product_view, name='delete_product_view'),
+    path('create/', create_product_view, name='create_product_view'),
+}
---
+++ l22/product/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def get_product_view(request, product_id):
+    pass
+
+def delete_product_view(request, product_id):
+    pass
+
+def create_product_view(request):
+    pass
