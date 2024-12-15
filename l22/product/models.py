1e458d5489f23d507008c16d31aab647219f24f6
dnxa <kuridze0@gmail.com>
2024-12-15 19:49:34 +0400

    Add product model

--- l22/product/models.py
+++ l22/product/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    quantity = models.IntegerField()
+    quality = models.IntegerField()
+
+    def __str__(self):
+        return self.name
